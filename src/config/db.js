6fa1c02f04d5b7a135bd182ed3710197dbcc67e9
fmarcos38 <fmarcos_23@hotmail.com>
2026-05-20 11:35:19 -0300

    nueva config mongo

--- src/config/db.js
+++ src/config/db.js
@@ -3,10 +3,7 @@ const mongoose = require('mongoose');
 
 const dbConnection = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log('Conectado a la base de datos MongoDB');
     } catch (error) {
         console.error('Error al conectar a la base de datos', error);
